941f29c0274a217c73e529410c63e7470226a1ef
Dominik Hrinkino <admin@hrinkino.com>
2022-04-24 08:07:06 +0200

    basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
+import axios from "axios";
 import { Component } from "react";
 class App extends Component {
   constructor(props) {
     super(props);
   }
 
+  state = { events: [] }
+
   render() { 
     return ( 
     <div>
-      We are live!
+      <div className="min-h-screen w-full h-screen bg-gradient-to-r from-green-300 via-cyan-400   to-blue-600">
+          <div className="flex h-screen justify-center items-center flex-col">
+            <div>
+              <p className="text-5xl font-bold text-white drop-shadow">Dominik Hrinkino</p>
+            </div>
+            <div>
+              <p className="text-xl text-white drop-shadow">FullStack Developer, DevOps,  SysAdmin</p>
+            </div>
+            
+          </div>        
+      </div>
+      <div className="min-h-screen bg-red-100">
+        <div className="py-5 px-5">
+          <div>
+
+            <p className="text-4xl">Niečo o mne</p>
+            <div className="grid md:grid-cols-2">
+              <div>1</div>
+              <div>2</div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div className="min-h-screen bg-green-400">
+        <div className="py-5 px-5">
+          <p className="text-4xl">O mne</p>
+        </div>
+      </div>
     </div>
        );
   }
