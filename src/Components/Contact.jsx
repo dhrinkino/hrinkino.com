be0d3705337ca10645e5e4640e2bf267c380480a
Dominik Hrinkino <admin@hrinkino.com>
2022-04-25 13:36:43 +0200

    added footer

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,23 +6,36 @@ class Contact extends Component {
     }
     state = {  }
     render() { 
-        return ( 
-        <div className="min-h-screen">
-            <div className="flex h-screen justify-center items-center flex-col">
-                <div>
-                    <p class="text-4xl font-bold">
-                        Zaujal som ťa? Neváhaj ma kontaktovať 🤩
-                    </p>
+        return (
+            <div>
+            <div className="min-h-screen bg-gray-200">
+                <div className="flex h-screen justify-center items-center flex-col">
+                    <div>
+                        <p class="text-4xl font-bold">
+                            Zaujal som ťa? Neváhaj ma kontaktovať 🤩
+                        </p>
+                    </div>
+                    <div class="flex justify-center text-6xl space-x-9 > * + *">
+                    <a className="text-black hover:text-gray-600" href="mailto:admin@hrinkino.com">
+                        <i className="fa fa-envelope" aria-hidden="true"></i>
+                    </a>
+                    <a className="text-black hover:text-gray-600" href="https://github.com/dhrinkino">
+                        <i class="fa fa-github" aria-hidden="true"></i>
+                    </a>
+                    </div>
+                </div>   
+            </div>
+
+            <div class="flex flex-col justify-between">
+                <div class="h-10 bg-gray-200 text-center text-sm text-gray-400">
+                    <div>
+                        Made by Dominik Hrinkino, licensed under MIT
+                    </div>
+                    <div>
+                        Did you know this site is available on <a href="https://github.com/dhrinkino/hrinkino.com">github?</a>
+                    </div>
                 </div>
-                <div class="w-screen flex justify-center text-6xl space-x-9 > * + *">
-                <a className="text-black hover:text-gray-600" href="mailto:admin@hrinkino.com">
-                    <i className="fa fa-envelope" aria-hidden="true"></i>
-                </a>
-                <a className="text-black hover:text-gray-600" href="https://github.com/dhrinkino">
-                    <i class="fa fa-github" aria-hidden="true"></i>
-                </a>
-                </div>
-            </div>   
+            </div>
         </div>
          );
     }
